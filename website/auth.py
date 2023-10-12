7f82b1171e30a5143b95e502b6c15708d5ba7695
Floresj12atwit <floresj12@wit.edu>
2023-10-11 23:33:15 -0400

    At 1 hour in the tutorial it is now a basic website with a nav bar but with no back end implementation the forms do not anything

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,20 @@
-from flask import Blueprint #Blueprint allows us to separate and organize our project, it has URLS defined in it 
-
+from flask import Blueprint, render_template, request #Blueprint allows us to separate and organize our project, it has URLS defined in it, render template allows us to render the templates we've created for the pages
+                                                      
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])          #Allows us to accept both GET and POST requests at both of these routes (only GET by default)
 def login():
-    return "<p>Login</p>"
+    data = request.form                 #request allows us to get information that was sent to access this route URL, method and data, data that was sent as a part of the form will be retrieved from this line
+    print(data)                         #notice the difference when empty POST request is sent (Pressing login) vs empty GET request is sent (referesshing the page)
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
 
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 '''
 This is going to hold pages the users can navigate to
 '''
-
-from flask import Blueprint #Blueprint allows us to separate and organize our project, it has URLS defined in it 
+                                             #render template allows us to render the templates we've created to hold the pages (the different files containing the web pages)
+from flask import Blueprint, render_template #Blueprint allows us to separate and organize our project, it has URLS defined in it 
 
 views = Blueprint('views', __name__)
 
@@ -10,6 +10,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/') #this decorates a function to register it with a given URL 
 def home():
-    return "<h1>TestingLol<h1>"
+    return render_template("home.html")
+
 
 
